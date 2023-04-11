931fe11d4ab48532e2ca788727bbeac5eaaabbb6
Talal <talalzafar84@gmail.com>
2023-04-11 16:39:00 +0500

    updtaed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Home from "@/components/Home/Home";
 import Navbar from "../components/navbar/navbar";
+import About from "../components/About/about";
 
 export default function page() {
   return (
   <div>
     <Navbar/>
     <Home/>
+    <About/>
   </div>
     )
 }
---
+++ components/About/about.tsx
@@ -0,0 +1,13 @@
+
+export default function about() {
+  return (
+    <div className="px-5 py-4 flex flex-col">
+        <h1 className="text-white font-bold text-2xl underline font-serif">ABOUT:</h1>
+        <div>
+            <p className="text-white font-Kurale">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto perferendis minus tenetur magni ratione dolorum repudiandae voluptate consequatur fugiat quidem, enim atque tempore quod quo numquam harum facilis. Quisquam, impedit.
+            </p>
+        </div>
+    </div>
+  )
+}
