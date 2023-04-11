6e6fdc1b422a34b959c7f28323860443ca1cb620
Talal <talalzafar84@gmail.com>
2023-04-11 13:29:35 +0500

    updtaed

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -8,15 +8,15 @@ type Props = {}
 
 export default function Home({}: Props) {
   return (
-      <div className='px-5 py-3 flex justify-around items-center h-screen'>
-        <div className="text-white">
-            <h1 className="md:text-6xl text-2xl font-Kurale">Hello! I’m</h1>
-            <br/>
-            <h1 className={`${styles.name} font-bold text-4xl md:text-8xl font-Kurale`}>Talal Zafar</h1>
-            <br/>
-            <h3 className="text-center md:text-3xl text-xl font-Kurale">MERN Stack Developr</h3>
+      <div className='px-5 py-3 flex flex-col-reverse'>
+        <div className="text-white mt-3 gap-3">
+            <h1 className="md:text-6xl text-3xl font-Kurale">Hello! I’m</h1>
+            <h2 className={`${styles.name}  leading-relaxed font-bold text-5xl md:text-8xl font-Kurale`}>Talal Zafar</h2>
+            <h3 className="text-xl font-Kurale ">MERN Stack Developr And Designer</h3>
         </div>
-    <Image src={profilePic}  alt="profile pic" className='h-80	w-auto'/>
+        <div className='flex justify-center mt-3'>
+    <Image src={profilePic}  alt="profile pic" className='h-2/6	w-auto'/>
+        </div>
     </div>
   )
 }
