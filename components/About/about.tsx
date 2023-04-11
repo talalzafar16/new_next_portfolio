06986e706f526a5984c2de8a59db7162b3bb6a12
Talal <talalzafar84@gmail.com>
2023-04-11 16:50:28 +0500

    updtaed

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+import pic from "../../public/second.jpg"
+
 
 export default function about() {
   return (
-    <div className="px-5 py-4 flex flex-col">
-        <h1 className="text-white font-bold text-2xl underline font-serif">ABOUT:</h1>
+    <div className="px-5 py-4 flex flex-col h-screen gap-6">
+        <h1 className="text-white font-bold text-center text-2xl underline font-serif ">ABOUT:</h1>
+        <Image src={pic} alt="profile" className='h-96 rounded-2xl ' />
         <div>
             <p className="text-white font-Kurale">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto perferendis minus tenetur magni ratione dolorum repudiandae voluptate consequatur fugiat quidem, enim atque tempore quod quo numquam harum facilis. Quisquam, impedit.
