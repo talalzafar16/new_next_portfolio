f25baffeec58b83dd98e09c28501717c28d523f1
Talal <talalzafar84@gmail.com>
2023-04-13 22:01:10 +0500

    Completed Responsivnes

--- components/Services/services.tsx
+++ components/Services/services.tsx
@@ -32,35 +32,35 @@ function Services() {
       <div className="w-full mt-4">
         <div className="flex group hover:border-3 justify-around hover:border-SecondaryColor md:gap-7 items-center border-2 w-full h-52 rounded-2xl p-4 sm:p-6">
           <div className="text-white flex flex-col ">
-            <h1 className="font-Kurale z-20 text-xl sm:text-2xl font-bold mb-4">
+            <h1 className="font-Kurale z-20 text-xl xl:text-3xl sm:text-2xl font-bold mb-4">
               Website Development
             </h1>
-            <p className="font-Kurale text-sm text-slate-200 mb-3 sm:text-base">
+            <p className="font-Kurale text-sm text-slate-200 xl:text-lg mb-3 sm:text-base">
               I build fully responsive and properly functional websites usnig
               latest technologies.
             </p>
-            <h2 className="font-Kurale sm:text-lg font-base font-bold group-hover:underline group-hover:underline-offset-2">10+ Projects</h2>
+            <h2 className="font-Kurale sm:text-lg font-base font-bold xl:text-xl group-hover:underline group-hover:underline-offset-2">10+ Projects</h2>
           </div>
           <span
-            style={width<641?{ fontSize: "49px" }:{ fontSize: "68px" }}
+            style={width<641?{ fontSize: "49px" }:width<1280?{ fontSize: "68px" }:{ fontSize: "88px" }}
             className="z-10 text-SecondaryColor material-icons"
           >
             code_off
           </span>
         </div>
-        <div className="flex group md:gap-7 items-center justify-around hover:border-3 sm:p-6 hover:border-SecondaryColor border-2 w-full h-52 rounded-2xl p-4 mt-10">
+        <div className="flex group md:gap-7 items-center justify-around  hover:border-3 sm:p-6 hover:border-SecondaryColor border-2 w-full h-52 rounded-2xl p-4 mt-10">
           <div className="text-white flex flex-col md:justify-between">
-            <h1 className="font-Kurale z-20 sm:text-2xl text-xl font-bold mb-4">
+            <h1 className="font-Kurale z-20 sm:text-2xl xl:text-3xl text-xl font-bold mb-4">
               App Development
             </h1>
-            <p className="font-Kurale text-sm text-slate-200 mb-3 sm:text-base">
+            <p className="font-Kurale text-sm text-slate-200 xl:text-lg mb-3 sm:text-base">
               I build fully operational and hybrid appliactions utilizing react
               native.
             </p>
-            <h2 className="group-hover:underline sm:text-lg font-Kurale font-base font-bold group-hover:underline-offset-2">6+ Projects</h2>
+            <h2 className="group-hover:underline sm:text-lg font-Kurale xl:text-xl font-base font-bold group-hover:underline-offset-2">6+ Projects</h2>
           </div>
           <span
-            style={width<641?{ fontSize: "49px" }:{ fontSize: "68px" }}
+            style={width<641?{ fontSize: "49px" }:width<1280?{ fontSize: "68px" }:{ fontSize: "88px" }}
             className="z-10 text-SecondaryColor material-icons"
           >
             developer_mode
